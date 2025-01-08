bd2d62fad59e3e0fc184e4aea0e1aebc3b938c81
Emma Perris <emma@Emmas-MacBook-Air.local>
2025-01-08 12:34:11 -0800

    Added code for the third segment.

--- code/main.py
+++ code/main.py
@@ -12,7 +12,7 @@ class Game:
 
         self.level = Level()
 
-    def run(self):
+    def run(self): 
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
