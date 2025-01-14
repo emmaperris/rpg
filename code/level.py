91cc1bcf6f07b88954c6a447ae38719f4f94dbbb
Emma Perris <emma@Emmas-MacBook-Air.local>
2025-01-14 10:06:19 -0800

    Added code for the sixth segment

--- code/level.py
+++ code/level.py
@@ -5,6 +5,7 @@ from player import Player
 from debug import debug
 from support import *
 from random import choice
+from weapon import Weapon
 
 class Level:
     def __init__(self):
@@ -13,6 +14,8 @@ class Level:
 
         self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
+
+        self.current_attack = None
 
         self.create_map()
 
@@ -47,8 +50,16 @@ class Level:
                             Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'object',surf)
 
 
-        self.player = Player((2000,1430),[self.visible_sprites], self.obstacle_sprites)        
+        self.player = Player((2000,1430),[self.visible_sprites], self.obstacle_sprites,self.create_attack,self.destroy_attack)        
     
+    def create_attack(self):
+        self.current_attack = Weapon(self.player,[self.visible_sprites])
+
+    def destroy_attack(self):
+        if self.current_attack:
+            self.current_attack.kill()
+        self.current_attack = None
+
     def run(self):
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
--- code/settings.py
+++ code/settings.py
@@ -2,3 +2,11 @@ WIDTH    = 1280
 HEIGHT   = 720
 FPS      = 60
 TILESIZE = 64
+
+weapon_data = {
+    'sword' : {'cooldown': 100, 'damage': 15, 'graphic':'Users/emma/src/rpg/graphics/weapons/sword/full.png'},
+    'lance' : {'cooldown': 400, 'damage': 30, 'graphic':'Users/emma/src/rpg/graphics/weapons/lance/full.png'},
+    'axe' : {'cooldown': 300, 'damage': 20, 'graphic':'Users/emma/src/rpg/graphics/weapons/axe/full.png'},
+    'rapier' : {'cooldown': 50, 'damage': 8, 'graphic':'Users/emma/src/rpg/graphics/weapons/rapier/full.png'},
+    'sai' : {'cooldown': 80, 'damage': 10, 'graphic':'Users/emma/src/rpg/graphics/weapons/sai/full.png'}
+}
